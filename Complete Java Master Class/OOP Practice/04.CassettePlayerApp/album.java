f923a7f0a1b95bae8074d67a9c2676f356431dfb
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-02-07 19:35:09 +0200

    Create album.java

---
+++ Complete Java Master Class/OOP Practice/04.CassettePlayerApp/album.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Album {
+    private String nameOfAlbum;
+    private ArrayList<Song> songs;
+
+    public Album(String name) {
+        this.nameOfAlbum = name;
+        this.songs = new ArrayList<>();
+    }
+
+    public void addSongToAlbum(String songName, String songDuration) {
+        Song song = new Song(songName, songDuration);
+        songs.add(song);
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public String getNameOfAlbum() {
+        return nameOfAlbum;
+    }
+}
