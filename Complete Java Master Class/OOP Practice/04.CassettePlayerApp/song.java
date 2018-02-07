ad79d2eb0c3829640c3b5cc6118e9c704e3044e5
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-02-07 19:35:22 +0200

    Create song.java

---
+++ Complete Java Master Class/OOP Practice/04.CassettePlayerApp/song.java
@@ -0,0 +1,18 @@
+public class Song {
+    private String songTitle;
+    private String songDuration;
+
+    public Song(String songTitle, String songDuration) {
+        this.songTitle = songTitle;
+        this.songDuration = songDuration;
+    }
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public String getSongDuration() {
+        return songDuration;
+    }
+}
+
